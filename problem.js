c95ed29bac3dc8d763717ff322a1a9285620dcb7
Sonja Farrell <sonjafarrell@gmail.com>
2024-10-31 13:17:07 -0700

    Create problem.js

---
+++ problem.js
@@ -0,0 +1,15 @@
+/*
+Create a function that simulates a simple password checking system. The function will take in a password and a user's input as parameters. It will then compare the user's input to the password.
+
+- If the user's input matches the password exactly, log out "Access Granted!"
+- If the user's input does not match, log out "Access Denied!"
+- If the user's input is "forgot", log out "Here is a hint"
+- If the user's input is "reset", log out "Let's reset your account"
+
+Invoke examples of the function that fullfil each condition listed above. Feel free to work locally using node, and then paste your code into this file when it runs.
+
+CHALLENGE
+- If the password set is "forgot" or "reset", aside from "Access Granted", also log: "This password you set should not be used because it glitches the system".
+- If the password is less than 5 characters, log out "Your password is too short!".
+*/
+
